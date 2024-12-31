e7b3dfed4f901569145afc2cba198653b3f3a5ec
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2024-12-31 15:34:32 +0900

    q613 commit

---
+++ Jungol/src/q613/Main.java
@@ -0,0 +1,25 @@
+package q613;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Student st = new Student();
+		Scanner sc = new Scanner(System.in);
+		
+		st.name = sc.next();
+		st.school = sc.next();
+		st.grade = sc.nextInt();
+		
+		System.out.println("Name: " + st.name);
+		System.out.println("School: " + st.school);
+		System.out.println("Grade: " + st.grade);
+	}
+
+}
+
+class Student {
+	String name, school;
+	int grade;
+}
