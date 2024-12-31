b485e206ec638be89e15cda96f2b9a2fb6e047c5
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2024-12-31 14:06:07 +0900

    q558 commit

---
+++ Jungol/src/q558/Main.java
@@ -0,0 +1,25 @@
+package q558;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] nums = new int[100];
+		int num;
+		
+		for (int i = 0; i < nums.length; i++) {
+			num = sc.nextInt();
+			
+			if (num == 0) break;
+			nums[i] = num;
+		}
+		
+		for (int i = nums.length - 1; i >= 0; i--) {
+			if (nums[i] == 0) continue;
+			System.out.print(nums[i] + " ");
+		}
+	}
+
+}
