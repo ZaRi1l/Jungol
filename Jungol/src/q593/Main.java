6768901ac97f0801ecea7ae4465930b7ef62456a
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-03 09:23:46 +0900

    q593 commit

---
+++ Jungol/src/q593/Main.java
@@ -0,0 +1,22 @@
+package q593;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n;
+		
+		while(true) {
+			System.out.print("ASCII code =? ");
+			n = sc.nextInt();
+			if (n < 33 || n > 127) break;
+			
+			System.out.printf("%c\n" , n);
+		}
+		
+		sc.close();
+	}
+
+}
