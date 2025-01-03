db25b2d4c26ff899b84d31c3c1d231204b6c9f1f
rbxo0 <rbxo0@DESKTOP-A1GKF28.mshome.net>
2025-01-03 15:40:34 +0900

    q506 commit

---
+++ Jungol/src/q506/Main.java
@@ -0,0 +1,12 @@
+package q506;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int height = 170;
+		double weight = 68.6;
+		
+		System.out.printf("My height\n%d\nMy weight\n%.6f", height, weight);
+	}
+
+}
