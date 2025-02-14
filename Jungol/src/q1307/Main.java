42cbfb0f9b2194c24e508c4c78b14ea85015c4a9
ZaRi1l <zari_09@naver.com>
2025-02-14 17:32:11 +0900

    q1307 살짝 수정

--- Jungol/src/q1307/Main.java
+++ Jungol/src/q1307/Main.java
@@ -14,7 +14,7 @@ public class Main {
 		StringBuffer sb = new StringBuffer();
 		for (int i = n - 1; i >= 0; i--) {
 			for (int j = n - 1; j >= 0; j--) {
-				int a = (j * n + i) % 26 + 65;
+				int a = (j * n + i) % 26 + 'A';
 				sb.append((char) a).append(' ');
 			}
 			sb.append('\n');
