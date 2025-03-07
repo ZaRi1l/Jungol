d8da79af247c97c566b5aab6faaabcb425d5504e
ZaRi1l <zari_09@naver.com>
2025-03-07 10:24:51 +0900

    q1169 commit

--- Jungol/src/q1169/Main.java
+++ Jungol/src/q1169/Main.java
@@ -26,7 +26,7 @@ public class Main {
 	}
 
 	private static void dfs(int idx) {
-		if (idx == dice.length) {
+		if (idx == dice.length) { // idx가 주사위 배열의 길이와 같다면 출력
 			for (int i = 0; i < dice.length; i++) {
 				sb.append(dice[i]).append(' ');
 			}
@@ -35,28 +35,28 @@ public class Main {
 		}
 
 		switch (m) {
-		case 1:
+		case 1: // M = 1 : 주사위를 N번 던져서 나올 수 있는 모든 경우
 			for (int i = 1; i <= 6; i++) {
 				dice[idx] = i;
 				dfs(idx + 1);
 			}
 			break;
-		case 2:
+		case 2: // M = 2 : 주사위를 N번 던져서 중복이 되는 경우를 제외하고 나올 수 있는 모든 경우
 			if (idx == 0)
 				for (int i = 1; i <= 6; i++) {
 					dice[idx] = i;
 					dfs(idx + 1);
 				}
 			else
-				for (int i = dice[idx - 1]; i <= 6; i++) {
+				for (int i = dice[idx - 1]; i <= 6; i++) { // 바로 앞에서 나왔던 숫자부터 넣어주면 중복이 안생김
 					dice[idx] = i;
 					dfs(idx + 1);
 				}
 			break;
-		case 3:
+		case 3: // M = 3 : 주사위를 N번 던져서 모두 다른 수가 나올 수 있는 모든 경우
 			for (int i = 1; i <= 6; i++) {
 				boolean in = false;
-				for (int j = 0; j < idx; j++) {
+				for (int j = 0; j < idx; j++) { // 앞에서 i가 나왔는지 검사
 					if (i == dice[j]) {
 						in = true;
 						break;
@@ -64,7 +64,7 @@ public class Main {
 				}
 				if (in)
 					continue;
-				
+
 				dice[idx] = i;
 				dfs(idx + 1);
 			}
